ca4e3c644bf1dca64214f2c7b04e32334292d488
kamran alam <kammykamran0093@gmail.com>
2025-07-31 16:48:38 +0530

    second commit

--- bubble_sort/bubble_sort.java
+++ bubble_sort/bubble_sort.java
@@ -8,17 +8,17 @@ public class bubble_sort {
             //for best case optimization
             // if no swaps are made, the array is already sorted
             // and we can exit early
-            boolean flag=true;
+            boolean flag=false;
             
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]= arr[j+1];
                     arr[j+1]=temp;
-                    flag=false;
+                    flag=true;
                 }
             }
-            if(flag){
+            if(!flag){
                 return;
             }
         }
