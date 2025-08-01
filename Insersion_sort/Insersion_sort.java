c671b488b43ec34d5c73e2adb5df516e015f2b4f
kamran alam <kammykamran0093@gmail.com>
2025-08-01 18:22:28 +0530

    insersion sort

---
+++ Insersion_sort/Insersion_sort.java
@@ -0,0 +1,26 @@
+package Insersion_sort;
+
+import java.util.Scanner;
+
+public class Insersion_sort {
+    static void insersion_sort(int []arr,int n){
+        for(int i=1;i<n;i++){
+            int j=i;{
+                while(j>0 && arr[j]<arr[j-1]){
+                    // Swap the elements if they are in the wrong order
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    j--;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]= {7,6,5,3,0,2,1};
+        insersion_sort(arr, arr.length);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
