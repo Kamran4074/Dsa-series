6bfbcfe0d747e6fff2f7a68e6ac8b636dd4e5758
kamran alam <kammykamran0093@gmail.com>
2025-08-03 23:47:46 +0530

    edit frog_jump

--- Recursion lecture 33/frog_jump.java
+++ Recursion lecture 33/frog_jump.java
@@ -1,16 +1,16 @@
 public class frog_jump {
 
-    static int best(int h[],int n,int idx){
-        if(idx==n-1){
+    static int best(int h[],int n,int index){
+        if(index==n-1){
             return 0;
         }
-        int opt1= Math.abs(h[idx]-h[idx+1])+best(h, n, idx+1);
+        int opt1= Math.abs(h[index]-h[index+1])+best(h, n, index+1);
 
         //idx=n-2 mean currently frog is standing on second last index so 
         //it can't jump 2 times from here(index out of bound)
-        if(idx==n-2) return opt1;
+        if(index==n-2) return opt1;
 
-        int opt2= Math.abs(h[idx]-h[idx+2])+best(h, n, idx+2);
+        int opt2= Math.abs(h[index]-h[index+2])+best(h, n, index+2);
         return Math.min(opt1, opt2);
     }
     public static void main(String[] args) {
